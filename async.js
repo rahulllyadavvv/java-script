dc67fdc9bc157bfef76e26b5a749f57aa038390d
Rahu Yadav <rahulvlogs25@gmail.com>
2025-08-15 16:57:39 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,11 @@
+// Default Javascript is a synchronous language
+//it is a single threaded language (everthing executes ona single thread)
+
+
+//execution context 
+//executes one line of code at a time
+//each operation waits for the last one to complete before executing
+
+//Two types of Codes
+//1.Blocking code-Block the flow of programm (Read file syncn)
+//2.Non Blocking Code -Does not block execution (Read file Async)
